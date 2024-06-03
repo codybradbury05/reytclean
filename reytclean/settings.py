e7fc985776e1efc8817d2d0b158ae6bd00bfc6c8
Cody Bradbury <codybradbury05@gmail.com>
2024-06-03 21:02:04 +0000

    Added deployment settings

--- reytclean/settings.py
+++ reytclean/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +29,7 @@ SECRET_KEY = 'django-insecure-7fs*fmki771sx$+r&j8qy+bhkyvs2)@^z!do6^u5hv6($8j&%8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
