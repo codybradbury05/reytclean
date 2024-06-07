7e0dbe417937b21eea6caf91f09dd59da9347845
Cody Bradbury <codybradbury05@gmail.com>
2024-06-07 06:39:43 +0000

    bug fix

--- reytclean/settings.py
+++ reytclean/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-codybradbury0-reytclean-n6y2vomx9ao.ws.codeinstitute-ide.net','.herokuapp.com', 'https://reytclean-9289dd45efa6.herokuapp.com/']
 
